95d86bf7411d9c7ddd1f92145cd01f229648c88d
Chad Danker <chaddanker@gmail.com>
2020-12-25 12:21:02 +0200

    mapbox-gl npm package added instead of cdn

--- src/utils/useMapBox.js
+++ src/utils/useMapBox.js
@@ -1,8 +1,9 @@
 import keys from '../config/keys';
+import mapboxgl from 'mapbox-gl';
 
 export const buildMap = (center, zoom, isDark, hasPosition) => {
-    window.mapboxgl.accessToken = keys.mapBoxKey;
-    const map = new window.mapboxgl.Map({
+    mapboxgl.accessToken = keys.mapBoxKey;
+    const map = new mapboxgl.Map({
       container: 'map', // container id
       style: `mapbox://styles/mapbox/${isDark ? 'dark' : 'light'}-v10`, // style URL
       center, // starting position [lng, lat]
@@ -13,7 +14,7 @@ export const buildMap = (center, zoom, isDark, hasPosition) => {
       map.resize();
     });
 
-    hasPosition ? new window.mapboxgl.Marker()
+    hasPosition ? new mapboxgl.Marker()
     .setLngLat(center)
     .addTo(map) : null;
 };
